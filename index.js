b114c32f41525da99d5be9862cd2070603dfd4c4
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-02 23:19:25 +0530

    ttrt

--- index.js
+++ index.js
@@ -1,58 +1,27 @@
 const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-// const authRoute = require('./routes/authRoute.js')
-// const usersRoute = require('./routes/usersRoute.js')
-// const hotelsRoute = require('./routes/hotelsRoute.js')
-// const roomsRoute = require('./routes/roomsRoute.js')
-const cookieParser = require('cookie-parser')
-require("dotenv").config()
+// const mongoose = require('mongoose')
 
 const app = express()
+const PORT = 5500
 
-app.use(cors())
-// is global middleware
-app.use(cookieParser())
-app.use(express.json())
+// app.use("/", (req, res) => {
+//     console.log("hello world")
+// }) // our routes
 
-// error handling middleware + global middleware
-// app.use((err, req, res, next) => {
-
-//     const errorStatus = "err.status" || 500
-//     const errorMessage = "err.message" || "Something went wrong!"
-
-//     return res.status(errorStatus).json({
-//         success: false,
-//         status: errorStatus,
-//         message: errorMessage,
-//         stack: err.stack,
-//     })
-// })
-
-
-
-app.use("/", () => {
-    console.log("hello world")
+app.use("/hi", (req, res) => {
+    res.json({ msg: "hi world" })
 }) // our routes
 
-app.use("/hi", () => {
-    console.log("hi world")
-}) // our routes
-// ROUTES
-// app.use("/api/auth", authRoute) // our routes
-// app.use("/api/users", usersRoute) // our routes
-// app.use("/api/hotels", hotelsRoute) // our routes
-// app.use("/api/rooms", roomsRoute) // our routes
 
 
-mongoose
-    .connect(process.env.MONGO_URL, { useNewUrlParser: true }, mongoose.set('strictQuery', true))
-    .then(() => console.log("MongoDB is connected"))
-    .catch((err) => console.log(err.message));
+// mongoose
+//     .connect(process.env.MONGO_URL, { useNewUrlParser: true }, mongoose.set('strictQuery', true))
+//     .then(() => console.log("MongoDB is connected"))
+//     .catch((err) => console.log(err.message));
 
 
 
-app.listen(process.env.PORT || 5000, (err) => {
+app.listen(PORT, (err) => {
     if (err) { throw err.message }
-    else { console.log(`espress app running on ${process.env.PORT || 5000}`); }
+    else { console.log(`espress app running on ${PORT}`); }
 })
