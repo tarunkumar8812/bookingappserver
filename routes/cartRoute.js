093d42a57c14854f2d8b98efc51928f15fb7d1be
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-03 12:03:22 +0530

    multiple routes

--- routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-
-router.get('/cartDetails', (req, res) => {
-    res.json({ msg: "user cart Details" })
-})
-
-module.exports = router
