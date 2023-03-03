49532ab9f3f56d3d46475369ae92b74177e6bed9
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-03 13:04:40 +0530

    added utils

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router()
 const { UpdateUser, deleteUser,
     getUser, getAllUsers } = require('../controllers/userController');
-// const { verifyToken, verifyUser, verifyAdmin } = require('../utils/verifyToken');
+const { verifyToken, verifyUser, verifyAdmin } = require('../utils/verifyToken');
 
 // PUT
-// router.put("/:id", verifyUser, UpdateUser)
+router.put("/:id", verifyUser, UpdateUser)
 
-// // DELETE
-// router.delete("/:id", verifyUser, deleteUser)
+// DELETE
+router.delete("/:id", verifyUser, deleteUser)
 
-// // GET single User by id
-router.get("/:id", getUser)
+// GET single User by id
+router.get("/:id", verifyUser, getUser)
 
 // GET All Users
-router.get("/getUser", getAllUsers)
+router.get("/", verifyAdmin, getAllUsers)
 
 module.exports = router
---
+++ utils/verifyToken.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+// const { createError } = require("../utils/error.js");
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token) {
+        return res.status(401).send({ status: false, message: "You are not authenticated!" });
+        // return next(createError(401, "You are not authenticated!"));
+    }
+
+    jwt.verify(token, process.env.JWT, (err, user) => {
+
+        if (err) return res.status(403).send({ status: false, message: "Token is not valid!" });
+
+        // if (err) return next(createError(403, "Token is not valid!"));
+        req.user = user;
+        next();
+    });
+};
+
+const verifyUser = (req, res, next) => {
+    verifyToken(req, res, next, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next();
+        } else {
+            return res.status(403).send({ status: false, message: "You are not authorized!" });
+            // return next(createError(403, "You are not authorized!"));
+        }
+    });
+};
+
+const verifyAdmin = (req, res, next) => {
+    verifyToken(req, res, next, () => {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            return res.status(403).send({ status: false, message: "You are not authorized!" });
+
+            // return next(createError(403, "You are not authorized!"));
+        }
+    });
+};
+
+
+
+module.exports = { verifyToken, verifyUser, verifyAdmin }
